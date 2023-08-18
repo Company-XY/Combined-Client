c2b810628700f2a3c4165f0369a8232c9f19efec
Olosko23 <oloogeorge633@gmail.com>
2023-08-18 13:29:32 +0300

    'register_login_functions'

--- src/pages/Register/Freelancer/freelancerRegister.jsx
+++ src/pages/Register/Freelancer/freelancerRegister.jsx
@@ -1,26 +1,84 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../../store/Slices/userSlice";
+import { Link, useNavigate } from "react-router-dom";
 import { FaUserShield } from "react-icons/fa";
 import { BsFillShieldLockFill, BsFillPersonCheckFill } from "react-icons/bs";
 import { AiOutlineSwapRight } from "react-icons/ai";
 
 const freelancerRegister = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [name, setName] = useState("");
+  const [consultation, setConsultation] = useState("");
+  const [type, setType] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (password === password2) {
+      try {
+        const response = await axios.post(
+          "https://auth-server-0bsp.onrender.com/api/v1/register/freelancer",
+          {
+            type,
+            name,
+            email,
+            password,
+            consultation,
+          }
+        );
+
+        const user = response.data;
+        setIsLoading(false);
+        dispatch(setUser(user));
+        navigate("/login");
+      } catch (error) {
+        console.error("Registration failed:", error);
+      }
+    } else {
+      setPasswordError("Passwords Do not Match");
+      setIsLoading(false);
+    }
+  };
+
   return (
     <main className="px-10 py-5 grid place-items-center w-full h-[90vh]">
       <section>
         <h2 className="text-center font-semibold text-2xl pb-2 mb-2">
-          Enter correct credentials to join Assist Africa as a Freelancer
+          Enter correct credentials to join Assist Africa as a Client
         </h2>
         <p className="py-2 mb-2">
-          Register as a Client instead{" "}
+          Register as a Freelancer instead{" "}
           <span>
-            <Link to="/register/client">
+            <Link to="/register/freelancer">
               {" "}
               <span className="font-semibold">Here</span>
             </Link>
           </span>
         </p>
-        <form>
-          <div className="flex flex-col gap-2 mb-2">
+        <form onSubmit={handleRegister}>
+          <div className="flex flex-col gap-2 py-2 mb-2">
+            <label>Account Type</label>
+            <select
+              className="px-4 py-2 border rounded-lg"
+              value={type}
+              required
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option>---</option>
+              <option value="Individual">Individual</option>
+              <option value="Business">Business</option>
+            </select>
+          </div>
+          <div className="flex flex-col gap-2 my-2">
             <label className="flex gap-2" htmlFor="name">
               {" "}
               <span>
@@ -33,6 +91,7 @@ const freelancerRegister = () => {
               type="text"
               required
               placeholder=""
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -48,7 +107,21 @@ const freelancerRegister = () => {
               type="email"
               required
               placeholder=""
+              onChange={(e) => setEmail(e.target.value)}
             />
+          </div>
+          <div className="my-3 py-2 flex flex-col gap-2">
+            <label>Do you Offer Consultation Services</label>
+            <select
+              className="px-4 py-2 border rounded-lg"
+              value={consultation}
+              required
+              onChange={(e) => setConsultation(e.target.value === "true")}
+            >
+              <option>---</option>
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
           </div>
           <div className="flex flex-col gap-2 py-2">
             <label className="flex gap-2 mt-2" htmlFor="password">
@@ -63,6 +136,7 @@ const freelancerRegister = () => {
               type="password"
               required
               placeholder=""
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="flex flex-col gap-2 mb-2 py-2">
@@ -78,8 +152,10 @@ const freelancerRegister = () => {
               type="password"
               required
               placeholder=""
+              onChange={(e) => setPassword2(e.target.value)}
             />
           </div>
+          <p className="text-red-400">{passwordError}</p>
           <div className="w-full text-center grid place-items-center">
             <button
               type="submit"
@@ -88,7 +164,13 @@ const freelancerRegister = () => {
               <span>
                 <AiOutlineSwapRight size={20} />
               </span>
-              <span className="items-center">Register</span>
+              <span className="items-center">
+                {isLoading ? (
+                  <span>Registration in Progress</span>
+                ) : (
+                  <span>Register</span>
+                )}
+              </span>
             </button>
           </div>
         </form>
